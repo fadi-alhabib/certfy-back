31fc5fe04556298492aa141b04da1b869336ee9d
jesus is king <fadi.b.alhabib@gmail.com>
2025-04-17 01:10:19 +0300

    Initial

--- app/Http/Services/MemoryImageService.php
+++ app/Http/Services/MemoryImageService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services\Common;
+namespace App\Http\Services;
 
 use App\Services\Common\Contracts\ImageServiceInterface;
 use Illuminate\Support\Facades\Storage;
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
+use App\Http\Services\MemoryImageService;
 use App\Services\Common\Contracts\ImageServiceInterface;
-use App\Services\Common\MemoryImageService;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
