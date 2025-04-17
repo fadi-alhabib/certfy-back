9a5d0b5ebda0a568751e5a5839c02bf3515f7b63
jesus is king <fadi.b.alhabib@gmail.com>
2025-04-17 18:15:26 +0300

    redirect api

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect("https://certfy.me", secure: true);
 });
 Route::get('/storage/{folder}/{filename}', function ($folder, $filename) {
     $path = storage_path('app/public/' . $folder . '/' . $filename);
